186cc82cbf850c913196e9e96c850fe31d396d66
thilinamh <thilinamh2013@gmail.com>
2014-10-09 21:18:41 +0530

    Update pingpair_dyn.cpp

--- RPi/RF24/examples/pingpair_dyn.cpp
+++ RPi/RF24/examples/pingpair_dyn.cpp
@@ -32,7 +32,9 @@ using namespace std;
 //RF24 radio(RPI_BPLUS_GPIO_J8_15,RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ);
 
 // Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 8Mhz
-RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_26, BCM2835_SPI_SPEED_8MHZ);
+
 
 
 // Radio pipe addresses for the 2 nodes to communicate.
